96d953383f00b6be60cc8f56cec2166b61f6d92f
Daniel <daniel.l.m.2442.123@gmail.com>
2021-07-11 13:26:21 +0200

    cositas

---
+++ DeepNN/ParameterUpdate/Stochastic.py
@@ -0,0 +1,12 @@
+from ParameterLearning import ParameterLearning
+import numpy as np
+
+class Stochastic(ParameterLearning):
+    def __init__(self,learningRate=0.01,rateDecay=0.001): 
+        super().__init__()
+        self.lrate=learningRate
+        self.rdecay=rateDecay
+
+    def UpdateParameter(self,W,B,dW,dB):
+        W-=self.lrate*dW
+        B-=self.lrate*dB
