b2ffd5f4c116f9ec840a438b2035d112e2431db3
mondovi <71011156+mondovi@users.noreply.github.com>
2021-07-15 19:53:49 +0200

    Update RMSProp.py

--- DeepNN/ParameterLearning/RMSProp.py
+++ DeepNN/ParameterLearning/RMSProp.py
@@ -1,2 +1,22 @@
+from ParameterLearning.ParameterLearning import ParameterLearning
+import numpy as np
 
+class AdaGrad(ParameterLearning):
+    def __init__(self,learningRate=0.01,rho=0.5):
+        super().__init__()
+        self.lrate=learningRate
+        self.rho=rho
         
+        self.init=True
+        
+    def UpdateParameter(self,W,B,dW,dB):
+        if self.init:
+            self.A_dW=np.zeros(W.shape)
+            self.A_dB=np.zeros(B.shape)
+            self.init=False
+        
+        self.A_dW = self.rho*self.A_dW + (1-self.rho)*np.square(dW)
+        self.A_dB = self.rho*self.A_dB + (1-self.rho)*np.square(dB)
+        
+        W -= (self.lrate*dW)/np.sqrt(self.A_dW)
+        B -= (self.lrate*dB)/np.sqrt(self.A_dB)
