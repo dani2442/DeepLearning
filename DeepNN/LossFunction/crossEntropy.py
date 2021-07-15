0671c3a5d8bc2d6428c12f0107e25013c5e542fa
Daniel <daniel.l.m.2442.123@gmail.com>
2021-07-15 19:30:05 +0200

    cross

--- DeepNN/LossFunction/crossEntropy.py
+++ DeepNN/LossFunction/crossEntropy.py
@@ -5,7 +5,7 @@ class crossEntropy(LossFunction):
     def __init__(self): super().__init__()
 
     @staticmethod
-    def Loss(output,y): return -(y*np.log(output)+(1-y)*np.log(1-output))
+    def Loss(output,y): return np.sum(-(y*np.log(output)+(1-y)*np.log(1-output)))
 
     @staticmethod
     def Backward(output,y): return -(y/output - (1-y)/(1-output))
