8a0a6ca770ae2d1918491304f6083a637dcb83d9
mondovi <71011156+mondovi@users.noreply.github.com>
2021-07-15 17:50:54 +0200

    Add files via upload

---
+++ DeepNN/LossFunction/MBE.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from LossFunction.LossFunction import LossFunction
+class MBE(LossFunction):
+    def __init__(self): super().__init__()
+
+    @staticmethod
+    def Loss(output,y): return np.sum(y-output)/len(output)
+
+    @staticmethod
+    def Backward(output,y): return -1/len(output)
