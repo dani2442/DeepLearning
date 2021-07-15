c0bb609664f7521c087626c0c45e9bcf71734aac
mondovi <71011156+mondovi@users.noreply.github.com>
2021-07-15 19:59:16 +0200

    Update AdaDelta.py

--- DeepNN/ParameterLearning/AdaDelta.py
+++ DeepNN/ParameterLearning/AdaDelta.py
@@ -2,10 +2,10 @@ from ParameterLearning.ParameterLearning import ParameterLearning
 import numpy as np
 
 class AdaGrad(ParameterLearning):
-    def __init__(self,learningRate=0.01,epsilon=0.001):
+    def __init__(self,learningRate=0.01,rho=0.5):
         super().__init__()
         self.lrate=learningRate
-        self.eps=epsilon
+        self.rho=rho
         
         self.init=True
         
@@ -13,10 +13,13 @@ class AdaGrad(ParameterLearning):
         if self.init:
             self.A_dW=np.zeros(W.shape)
             self.A_dB=np.zeros(B.shape)
+            self.delta_W, self-delta_B = 0,0
             self.init=False
         
-        self.A_dW += np.square(dW)
-        self.A_dB += np.square(dB)
+        self.A_dW = self.rho*self.A_dW + (1-self.rho)*np.square(dW)
+        self.A_dB = self.rho*self.A_dB + (1-self.rho)*np.square(dB)
         
-        W -= (self.lrate*dW)/np.sqrt(self.A_dW+self.eps)
-        B -= (self.lrate*dB)/np.sqrt(self.A_dB+self.eps)
+        self.delta = self.rho*self.rho +(1-self.rho)*np.square((self.lrate*dW)/np.sqrt(self.A_dW))
+        
+        W -= (self.lrate*dW)/np.sqrt(self.A_dW)
+        B -= (self.lrate*dB)/np.sqrt(self.A_dB)
