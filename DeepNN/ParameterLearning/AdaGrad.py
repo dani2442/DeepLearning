3f344db24d5c44cb0b46c9d12fa3dc3a85bbe808
mondovi <71011156+mondovi@users.noreply.github.com>
2021-07-15 18:11:32 +0200

    Update AdaGrad.py

--- DeepNN/ParameterLearning/AdaGrad.py
+++ DeepNN/ParameterLearning/AdaGrad.py
@@ -7,16 +7,16 @@ class AdaGrad(ParameterLearning):
         self.lrate=learningRate
         self.eps=epsilon
         
+        self.init=True
+        
     def UpdateParameter(self,W,B,dW,dB):
+        if self.init:
+            self.A_dW=np.zeros(W.shape)
+            self.A_dB=np.zeros(B.shape)
+            self.init=False
         
-        A_dW=np.zeros(W.shape)
-        A_dB=np.zeros(B.shape)
+        self.A_dW += dW**2
+        self.A_dB += dB**2
         
-        A_dW += dW**2
-        A_dB += dB**2
-        
-        W -= (self.lrate*dW)/np.sqrt(A_dW-self.eps)
-        B -= (self.lrate*dB)/np.sqrt(A_dB-self.eps)
-        
-        # los valores de A_dW y A_dB deben inicializarse en la 
-        # primera iteración y guardarse después de cada iteración
+        W -= (self.lrate*dW)/np.sqrt(self.A_dW-self.eps)
+        B -= (self.lrate*dB)/np.sqrt(self.A_dB-self.eps)
