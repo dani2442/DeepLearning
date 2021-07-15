064e42c1de6651d5ab1766ee65c69c74d1f5577c
mondovi <71011156+mondovi@users.noreply.github.com>
2021-07-15 20:50:28 +0200

    Update AdaDelta.py

--- DeepNN/ParameterLearning/AdaDelta.py
+++ DeepNN/ParameterLearning/AdaDelta.py
@@ -2,10 +2,11 @@ from ParameterLearning.ParameterLearning import ParameterLearning
 import numpy as np
 
 class AdaDelta(ParameterLearning):
-    def __init__(self,learningRate=0.01,rho=0.5):
+    def __init__(self,learningRate=0.01,rho=0.5,epsilon=0.001):
         super().__init__()
         self.lrate=learningRate
         self.rho=rho
+        self.eps=epsilon
         
         self.init=True
         
@@ -23,7 +24,7 @@ class AdaDelta(ParameterLearning):
         self.delta_W = self.rho*self.delta_W +(1-self.rho)*np.square((self.lrate*dW)/np.sqrt(self.A_dW))
         self.delta_B = self.rho*self.delta_B +(1-self.rho)*np.square((self.lrate*dB)/np.sqrt(self.A_dB))
         
-        W -= dW/np.sqrt(self.delta_W/self.A_dW)
-        B -= dB/np.sqrt(self.delta_B/self.A_dB)
+        W -= dW/np.sqrt(self.delta_W/self.A_dW + self.eps)
+        B -= dB/np.sqrt(self.delta_B/self.A_dB + self.eps)
 
         return W,B
